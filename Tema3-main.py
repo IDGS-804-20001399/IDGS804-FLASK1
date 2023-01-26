4e45c12f497f17a8307b26a88e2cdd3dc81d3432
Roberto Aguilera <robert712.calisthenics@gmail.com>
2023-01-25 19:39:58 -0600

    temas 1 al 3

---
+++ Tema3-main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "!Hola Mundo!"
+
+# Pasamos un string
+@app.route("/user/<string:user>")
+def user(user):
+    return f"Hola {user}"
+
+# Pasamos parametros int
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f"Numero: {n}"
+
+# Pasamos mas de un parametro
+@app.route("/user/<int:id>/<string:username>")
+def usern(id, username):
+    return f"ID: {id} nombre: {username}"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
